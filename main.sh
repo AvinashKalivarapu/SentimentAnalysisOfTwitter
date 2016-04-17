5cd11903b8f42220f1f593f821b9a700ba1bac60
akhilbatra898 <akhil.batra898@gmail.com>
2016-04-17 19:22:29 +0530

    main.sh bulk test data option updated

--- main.sh
+++ main.sh
@@ -4,4 +4,8 @@ python preprocess.py traintest.tsv preprocessedTweets.txt
 echo "Preprocessing successfull !! (Saved to preprocessedTweets.txt and preprocessedTweets_vs_Feature.txt)"
 #python preprocess.py testing.tsv testpreprocessedTweets.txt
 #echo "Preprocessing successfull !! (Saved to testpreprocessedTweets.txt)"
-python unigramSVM.py $1
+if [ $# -lt 2 ]; then
+  python unigramSVM.py $1
+else
+  python unigramSVM.py $1 $2
+fi
